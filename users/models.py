c769085fe70cbf28cff70616ac84292c7e09c704
Shei-ly <109236358+Shei-ly@users.noreply.github.com>
2022-07-14 10:35:13 -0500

    Diccionario de Información de Asesores

--- users/models.py
+++ users/models.py
@@ -5,7 +5,13 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-
+    usuario = models.CharField(max_length=50 )
+    auth = models.CharField(max_length=50)
+    nombreCompleto = models.CharField(max_length=150)
+    area = models.CharField(max_length=50)
+    ubicacion = models.CharField(max_length=50)
+    segmento = models.SmallIntegerField()
+    perfil = models.CharField(max_length=50)
     
     def get_full_name(self):
         return f'{self.first_name}{self.last_name}'
